fbf0db578fcc4b8256385e49ef8ff0a0650d6b1e
Carlos Dubón <holacarlosdubon@gmail.com>
2022-04-23 11:12:57 -0600

    update header

--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -1,14 +1,20 @@
 import { FC } from "react";
-import { PageHeader } from "antd";
 import { pageTitle } from "$config/site";
+import { Typography } from "antd";
+import Image from "next/image";
+const { Title, Text } = Typography;
 
 const Header: FC = () => {
   return (
-    <PageHeader
-      className="border-[1px] border-gray-200"
-      title={pageTitle}
-      subTitle={`Bienvenido a ${pageTitle}`}
-    />
+    <div className="flex items-center border-[1px] border-gray-200 w-full h-16 px-10">
+      <div className="mr-2 h-full grid place-items-center">
+        <Image src={"/logo.svg"} alt={"logo"} width={40} height={30} priority />
+      </div>
+      <Title level={4} className="!m-0">
+        {pageTitle}
+      </Title>
+      <Text className="!text-gray-400 ml-4">Bienvenido a {pageTitle}</Text>
+    </div>
   );
 };
 
