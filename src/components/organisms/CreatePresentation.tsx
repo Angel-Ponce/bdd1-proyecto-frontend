f61bf4a9c52c7d6997b95c906fac1bbe1f0537dc
Carlos Dubón <holacarlosdubon@gmail.com>
2022-04-29 16:52:06 -0600

    ability to close the modal

--- src/components/organisms/CreatePresentation.tsx
+++ src/components/organisms/CreatePresentation.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable no-unused-vars */
 import { Product } from "$store/slices/productsSlice";
-import { Dispatch, FC, SetStateAction } from "react";
+import { Button } from "antd";
+import { Dispatch, FC, SetStateAction, useState } from "react";
 
 interface Props {
   modalType: "create" | "edit";
@@ -12,7 +14,27 @@ const CreatePresentation: FC<Props> = ({
   product,
   setShowModal,
 }) => {
-  return <h1>{modalType == "create" ? "CREAR" : "EDIT"}</h1>;
+  const [savingPresentation, setSavingPresentation] = useState<boolean>(false);
+
+  return (
+    <div>
+      <div className="flex gap-2">
+        <Button
+          disabled={savingPresentation}
+          onClick={() => setShowModal(false)}
+        >
+          Cancelar
+        </Button>
+        <Button
+          type="primary"
+          loading={savingPresentation}
+          onClick={() => console.log("s")}
+        >
+          Guardar
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePresentation;
